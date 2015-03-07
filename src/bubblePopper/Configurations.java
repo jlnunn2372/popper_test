80da12f132cb41c8afeaaad52d7baec2b5c1548a
unknown <bpaxson62@gmail.com>
2015-03-07 00:56:58 -0500

    Created borderpane concepet with toolbar Created random bubbles popping up on screen and their transitions Need to move contents out of main

--- src/bubblePopper/Configurations.java
+++ src/bubblePopper/Configurations.java
@@ -3,14 +3,14 @@ package bubblePopper;
 public final class Configurations {
 	
 	//Window Sizes
-	public static final int MAIN_SCREEN_WIDTH;
-	public static final int MAIN_SCREEN_HEIGHT;
-	public static final int SCORE_SCREEN_WIDTH;
-	public static final int SCORE_SCREEN_HEIGHT;
-	public static final int WINDOW_BOARDER;
-	public static final int TITLE_BAR_HEIGHT;
+	public static final int MAIN_SCREEN_WIDTH=0;
+	public static final int MAIN_SCREEN_HEIGHT=0;
+	public static final int SCORE_SCREEN_WIDTH=0;
+	public static final int SCORE_SCREEN_HEIGHT=0;
+	public static final int WINDOW_BOARDER=0;
+	public static final int TITLE_BAR_HEIGHT=0;
 	
 	//Difficulty settings
-	public static final int LIFE_POINTS;
+	public static final int LIFE_POINTS=0;
 
 }
