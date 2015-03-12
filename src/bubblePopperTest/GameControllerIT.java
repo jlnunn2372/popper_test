8f04476e4d7a9172183f5fd6f0198793208d4dc5
jlnunn2372 <jlnunn2372@eagle.fgcu.edu>
2015-03-12 16:59:26 -0400

    Added IT test for gamecontroller and scoreboardcontroller

---
+++ src/bubblePopperTest/GameControllerIT.java
@@ -0,0 +1,15 @@
+package bubblePopperTest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameControllerIT {
+	
+	@Test
+	public static void gameStartTest() throws RuntimeException{
+		
+	}
+
+}
---
+++ src/bubblePopperTest/ScoreControllerIT.java
@@ -0,0 +1,14 @@
+package bubblePopperTest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ScoreControllerIT {
+	
+	@Test
+	public static void scoreBoardStartItTest() throws RuntimeException{
+		
+	}
+
+}
