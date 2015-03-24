d4236d9be773dd68cf566f0379cc633d8545fa4f
jlnunn2372 <jlnunn2372@eagle.fgcu.edu>
2015-03-23 20:50:35 -0400

    Changed test files to throw exception in method

--- src/bubblePopperTest/GifCreateTest.java
+++ src/bubblePopperTest/GifCreateTest.java
@@ -8,27 +8,28 @@ import static org.junit.Assert.*;
 public class GifCreateTest {
 	
 	@Before
-	public static void setup() throws RuntimeException{
+	public static void setup(){
 		//put setup info here
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
 	public void testGifCreate(){
 		//test if gif file is created
-	
+		throw new RuntimeException();
 	}
 	
 	@Test
 	public void testGifSaveLocation(){
 		//test to make sure gif saves to correct location
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
-	public void testGifSize() throws RuntimeException{
+	public void testGifSize(){
 		//test to make sure there is something in the gif file
 		//gif should not be 0bits in size
+		throw new RuntimeException();
 	}
 
 }
--- src/bubblePopperTest/LevelTest.java
+++ src/bubblePopperTest/LevelTest.java
@@ -9,50 +9,51 @@ import static org.junit.Assert.*;
 public class LevelTest {
 	
 	@Before
-	public void setup() throws RuntimeException{
+	public void setup() {
 			//put setup info here
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
-	public void testBubbleCreate() throws RuntimeException{
+	public void testBubbleCreate() {
 		// Test for creating a bubble
 		//Should create bubble of radius=RADIUS at coordinates supplied
 
 	}
 	
 	@Test
-	public void testBubbleMinBoundries() throws RuntimeException{
+	public void testBubbleMinBoundries() {
 		//Test for minimum distance from walls and other bubbles
 		//before creating bubble at locations
+		throw new RuntimeException();
 		
 	}
 	
 	@Test
-	public void testBubbleClicked() throws RuntimeException{
+	public void testBubbleClicked() {
 		//Test for bubble when user clicks it
 		//Bubble should pop and user be awarded points
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
-	public void testBubbleHitsWall() throws RuntimeException{
+	public void testBubbleHitsWall() {
 		//Test for when bubble hits a wall
 		//Bubbble should pop and user loses life
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
-	public void testBubbleHitsBubble() throws RuntimeException{
+	public void testBubbleHitsBubble() {
 		//Test for when bubbble hits another bubble
 		//Both bubbles should pop and user loses life twice
-		
+		throw new RuntimeException();
 	}
 	
 	@Test
-	public void testRandom() throws RuntimeException{
+	public void testRandom() {
 		//Test random points creation
 		//Should not receive same points over and over again
-		
+		throw new RuntimeException();
 	}
 }
